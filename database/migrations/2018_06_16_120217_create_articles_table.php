f21b10af4574618967f0e8a3c02b72b1ef456208
Thierry Dexter <thierryKweni@gmail.com>
2018-06-16 14:17:38 +0200

    Create table

---
+++ database/migrations/2018_06_16_120217_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('fichier_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('fichier_id')->references('id')->on('fichiers');
+            $table->string('titre');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
